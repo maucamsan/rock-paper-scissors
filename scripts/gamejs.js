04c205009e8e0556c179bd955fe08da0001191cb
Mauricio <maucamsan@gmail.com>
2022-04-04 18:06:36 -0500

    Add playRound function and simulate a case
    The function takes two arguments and turns them to lower case strings,
    them it proceeds to check one value against the other to determine a
    winner.

--- scripts/gamejs.js
+++ scripts/gamejs.js
@@ -1,5 +1,25 @@
 function computerPlay(){
     const actions = ["Rock", "Paper", "Scissors"];
-    return actions[Math.floor(Math.random() * (3))]
+    return actions[Math.floor(Math.random() * (3))].toLowerCase();
 }
 
+function playRound(playerSelection, computerSelection){
+    const pSelection = playerSelection.toLowerCase()
+    const cSelection = computerSelection.toLowerCase()
+    // Rock -> scissors, Scissors -> Paper, Paper -> Rock
+    if (pSelection === "rock"){
+        if (cSelection === "rock"){
+            return "tie"
+        }
+        else if (cSelection === "paper"){
+            return `You lose! ${cSelection} beats ${pSelection}`
+        }
+        else{
+            return `You win! ${pSelection} beats ${cSelection}`
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
